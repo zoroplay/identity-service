a7a742e4106d9484f568235863adaa7eb6b36a08
Isaac Williams <Isaaciyaye174@gmail.com>
2023-12-25 10:14:00 +0100

    New additions

--- src/proto/auth.pb.ts
+++ src/proto/auth.pb.ts
@@ -14,12 +14,14 @@ export interface UserData {
 /** send otp */
 export interface SendOtpRequest {
   username: string;
+  type: string;
 }
 
 export interface SendOtpResponse {
   status: number;
-  message: string[];
-  error: string[];
+  success: boolean;
+  message: string;
+  error: string;
 }
 
 /** Register */
@@ -31,7 +33,7 @@ export interface SportBookRegisterRequest {
 
 export interface SportBookRegisterResponse {
   status: number;
-  error: string[];
+  error: string;
   data: UserData | undefined;
 }
 
@@ -43,7 +45,7 @@ export interface LoginRequest {
 
 export interface LoginResponse {
   status: number;
-  error: string[];
+  error: string;
   token: string;
   data: UserData | undefined;
 }
@@ -55,8 +57,52 @@ export interface ValidateRequest {
 
 export interface ValidateResponse {
   status: number;
-  error: string[];
+  error: string;
   userId: number;
+}
+
+export interface ClientRequest {
+  name: string;
+  country: string;
+  currency: string;
+  website: string;
+  contactNumber: string;
+  contactEmail: string;
+  clientID: string;
+}
+
+export interface RemoveClientRequest {
+  clientID: string;
+}
+
+export interface RemoveRoleRequest {
+  roleID: string;
+}
+
+export interface RemovePermissionRequest {
+  permissionID: string;
+}
+
+export interface RoleRequest {
+  name: string;
+  description: string;
+  roleID: string;
+}
+
+export interface PermissionRequest {
+  name: string;
+  description: string;
+  permissionID: string;
+}
+
+export interface CommonResponse {
+  status: boolean;
+  message: string;
+  data: string[];
+  errors?: string | undefined;
+}
+
+export interface EmptyRequest {
 }
 
 export const AUTH_PACKAGE_NAME = "auth";
@@ -69,6 +115,24 @@ export interface AuthServiceClient {
   validate(request: ValidateRequest): Observable<ValidateResponse>;
 
   sendOtp(request: SendOtpRequest): Observable<SendOtpResponse>;
+
+  createClient(request: ClientRequest): Observable<CommonResponse>;
+
+  createPermission(request: PermissionRequest): Observable<CommonResponse>;
+
+  createRole(request: RoleRequest): Observable<CommonResponse>;
+
+  findAllPermissions(request: EmptyRequest): Observable<CommonResponse>;
+
+  findAllClients(request: EmptyRequest): Observable<CommonResponse>;
+
+  findAllRoles(request: EmptyRequest): Observable<CommonResponse>;
+
+  removeClient(request: RemoveClientRequest): Observable<CommonResponse>;
+
+  removeRole(request: RemoveRoleRequest): Observable<CommonResponse>;
+
+  removePermission(request: RemovePermissionRequest): Observable<CommonResponse>;
 }
 
 export interface AuthServiceController {
@@ -81,11 +145,45 @@ export interface AuthServiceController {
   validate(request: ValidateRequest): Promise<ValidateResponse> | Observable<ValidateResponse> | ValidateResponse;
 
   sendOtp(request: SendOtpRequest): Promise<SendOtpResponse> | Observable<SendOtpResponse> | SendOtpResponse;
+
+  createClient(request: ClientRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  createPermission(request: PermissionRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  createRole(request: RoleRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  findAllPermissions(request: EmptyRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  findAllClients(request: EmptyRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  findAllRoles(request: EmptyRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  removeClient(request: RemoveClientRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  removeRole(request: RemoveRoleRequest): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
+
+  removePermission(
+    request: RemovePermissionRequest,
+  ): Promise<CommonResponse> | Observable<CommonResponse> | CommonResponse;
 }
 
 export function AuthServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ["sportRegister", "login", "validate", "sendOtp"];
+    const grpcMethods: string[] = [
+      "sportRegister",
+      "login",
+      "validate",
+      "sendOtp",
+      "createClient",
+      "createPermission",
+      "createRole",
+      "findAllPermissions",
+      "findAllClients",
+      "findAllRoles",
+      "removeClient",
+      "removeRole",
+      "removePermission",
+    ];
     for (const method of grpcMethods) {
       const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
       GrpcMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
