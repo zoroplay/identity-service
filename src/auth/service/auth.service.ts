1ce206d07dbe4dd95fe0e4b6c1cf2a188fb0a01f
Isaac Williams <Isaaciyaye174@gmail.com>
2024-01-02 10:03:57 +0100

    new

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -2,70 +2,116 @@ import { HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from './jwt.service';
-import { RegisterRequestDto, LoginRequestDto, ValidateRequestDto } from '../auth.dto';
-import { LoginResponse, SportBookRegisterResponse, ValidateResponse } from 'src/proto/auth.pb';
+import {
+  RegisterRequestDto,
+  LoginRequestDto,
+  ValidateRequestDto,
+} from '../auth.dto';
+import {
+  LoginResponse,
+  SportBookRegisterResponse,
+  ValidateResponse,
+} from 'src/proto/auth.pb';
 import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-    @InjectRepository(User)
-    private readonly repository: Repository<User>;
+  @InjectRepository(User)
+  private readonly repository: Repository<User>;
 
-    @Inject(JwtService)
-    private readonly jwtService: JwtService;
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
 
-    public async register({ username, password }: RegisterRequestDto): Promise<SportBookRegisterResponse> {
-        let user: User = await this.repository.findOne({ where: { username } });
+  public async register({
+    username,
+    password,
+  }: RegisterRequestDto): Promise<SportBookRegisterResponse> {
+    let user: User = await this.repository.findOne({ where: { username } });
 
-        if (user) {
-            return { status: HttpStatus.CONFLICT, error: ['E-Mail already exists'], data: null };
-        }
-
-        user = new User();
-
-        user.username = username;
-        user.password = this.jwtService.encodePassword(password);
-
-        await this.repository.save(user);
-
-        return { status: HttpStatus.CREATED, error: null, data: user };
+    if (user) {
+      return {
+        status: HttpStatus.CONFLICT,
+        error: 'E-Mail already exists',
+        data: null,
+      };
     }
 
-    public async login({ username, password }: LoginRequestDto): Promise<LoginResponse> {
-        try {
-            const auth: User = await this.repository.findOne({ where: { username } });
+    user = new User();
 
-            if (!auth) {
-                return { status: HttpStatus.NOT_FOUND, error: ['E-Mail not found'], token: null, data: null };
-            }
+    user.username = username;
+    user.password = this.jwtService.encodePassword(password);
 
-            const isPasswordValid: boolean = this.jwtService.isPasswordValid(password, auth.password);
+    await this.repository.save(user);
 
-            if (!isPasswordValid) {
-                return { status: HttpStatus.NOT_FOUND, error: ['Password wrong'], token: null, data: null };
-            }
+    return { status: HttpStatus.CREATED, error: null, data: user };
+  }
 
-            const token: string = this.jwtService.generateToken(auth);
+  public async login({
+    username,
+    password,
+  }: LoginRequestDto): Promise<LoginResponse> {
+    try {
+      const auth: User = await this.repository.findOne({ where: { username } });
 
-            return { token, status: HttpStatus.OK, error: null, data: auth };
-        } catch (err) {
-            return { status: HttpStatus.NOT_FOUND, error: ['Something went wrong', err.message], token: null, data: null };
+      if (!auth) {
+        return {
+          status: HttpStatus.NOT_FOUND,
+          error: 'E-Mail not found',
+          token: null,
+          data: null,
         };
+      }
+
+      const isPasswordValid: boolean = this.jwtService.isPasswordValid(
+        password,
+        auth.password,
+      );
+
+      if (!isPasswordValid) {
+        return {
+          status: HttpStatus.NOT_FOUND,
+          error: 'Password wrong',
+          token: null,
+          data: null,
+        };
+      }
+
+      const token: string = this.jwtService.generateToken(auth);
+
+      return { token, status: HttpStatus.OK, error: null, data: auth };
+    } catch (err) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        error: 'Something went wrong ' + err.message,
+        token: null,
+        data: null,
+      };
+    }
+  }
+
+  public async validate({
+    token,
+  }: ValidateRequestDto): Promise<ValidateResponse> {
+    const decoded: User = await this.jwtService.verify(token);
+
+    if (!decoded) {
+      return {
+        status: HttpStatus.FORBIDDEN,
+        error: 'Token is invalid',
+        userId: null,
+      };
     }
 
-    public async validate({ token }: ValidateRequestDto): Promise<ValidateResponse> {
-        const decoded: User = await this.jwtService.verify(token);
+    const auth: User = await this.jwtService.validateUser(decoded);
 
-        if (!decoded) {
-            return { status: HttpStatus.FORBIDDEN, error: ['Token is invalid'], userId: null };
-        }
-
-        const auth: User = await this.jwtService.validateUser(decoded);
-
-        if (!auth) {
-            return { status: HttpStatus.CONFLICT, error: ['User not found'], userId: null };
-        }
-
-        return { status: HttpStatus.OK, error: null, userId: decoded.id };
+    if (!auth) {
+      return {
+        status: HttpStatus.CONFLICT,
+        error: 'User not found',
+        userId: null,
+      };
     }
+
+    return { status: HttpStatus.OK, error: null, userId: decoded.id };
+  }
 }
