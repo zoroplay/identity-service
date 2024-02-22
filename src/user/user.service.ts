355b0fe080124e907937f8583e353cdf3b91f81f
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-02-22 11:09:16 +0100

    update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,7 +6,8 @@ import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { handleError, handleResponse } from 'src/common/helpers';
 import { TrackierService } from './trackier/trackier.service';
-import { CreateUserRequest } from 'src/proto/identity.pb';
+import { CreateUserRequest, Player, SearchPlayerRequest, SearchPlayerResponse } from 'src/proto/identity.pb';
+import { WalletService } from 'src/wallet/wallet.service';
 
 @Injectable()
 export class UserService {
@@ -14,6 +15,7 @@ export class UserService {
     private prisma: PrismaService,
     private jwtService: JwtService,
     private trackierService: TrackierService,
+    private readonly walletService: WalletService,
   ) {}
 
   async saveAdminUser(data: CreateUserRequest) {
@@ -278,8 +280,66 @@ export class UserService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async searchPlayer({clientId, searchKey}: SearchPlayerRequest): Promise<SearchPlayerResponse> {
+    const key = `%${searchKey.toLowerCase()}%`
+      try {
+          const users: any = await this.prisma.$queryRaw`SELECT u.id, u.username, u.code, u.created_at, u.status, u.verified,
+          d.email, d.phone, d.firstName, d.lastName, d.country, d.currency, r.name as role
+          FROM users u 
+          LEFT JOIN user_details d ON u.id = d.user_id
+          LEFT JOIN roles r ON r.id = u.role_id
+          WHERE u.clientId = ${clientId} AND
+          d.firstName LIKE ${key} OR d.lastName LIKE ${key}
+          OR LOWER(d.phone) LIKE ${key}
+          OR LOWER(u.username) LIKE ${key}
+          OR LOWER(u.code) LIKE ${key}
+          OR LOWER(d.email) LIKE ${key}`;
+
+          const data = [];
+
+          console.log(users)
+          if (users.length > 0) {
+            for (const user of users) {
+              const userObject: Player = {
+                id: user.id,
+                code: user.code,
+                username: user.username,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                phoneNumber: user.phone,
+                registered: user.created_at,
+                country: user.country,
+                currency: user.currency,
+                status: user.status,
+                verified: user.verified,
+                balance: 0,
+                bonus: 0,
+                lifeTimeDeposit: 0,
+                lifeTimeWithdrawal: 0,
+                openBets: 0,
+                role: user.role
+              }
+              //get user wallet
+              const balanceRes = await this.walletService.getWallet({
+                userId: user.id,
+                clientId,
+              }).toPromise();
+
+              if(balanceRes.success){
+                const {balance, availableBalance, sportBonusBalance, casinoBonusBalance, virtualBonusBalance, trustBalance } = balanceRes.data
+                  userObject.balance = availableBalance;
+                  user.bonus = sportBonusBalance + casinoBonusBalance + virtualBonusBalance;
+                }
+              data.push(userObject);
+            }
+          }
+
+          return {success: true, message: "record fetched", data}
+      } catch (e) {
+        console.log('Error: ' + e.message);
+        return {success: false, message: "error fetching records", data: []}
+      }
   }
 
   async createShopUser(updateUserDto: UpdateUserDto & LoginDto) {
