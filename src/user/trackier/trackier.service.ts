2de187c7000c3895e4e82bf2818477e7a000fafe
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-09-02 11:47:22 +0100

    updates for trackier keys

--- src/user/trackier/trackier.service.ts
+++ src/user/trackier/trackier.service.ts
@@ -2,69 +2,108 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import * as dayjs from 'dayjs';
 import { handleError } from 'src/common/helpers';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TrackierService {
   protected baseUrl = 'https://api.trackierigaming.io';
 
+  constructor(
+    private prisma: PrismaService,
+  ) {}
+
+  async getKeys(clientId) {
+    let apiKeyQ = await this.prisma.setting.findFirst({
+      where: {
+        clientId,
+        option: `trackier_api_key`,
+      },
+    });
+
+    let authCodeQ = await this.prisma.setting.findFirst({
+      where: {
+        clientId,
+        option: `trackier_auth_code`,
+      },
+    });
+
+    let success = true, message = 'Success'
+
+    if (!apiKeyQ.value) {
+      success = false;
+      message = 'Not available'
+    }
+
+    return {
+      success, 
+      message,
+      data: {
+        ApiKey: apiKeyQ?.value || null,
+        AuthCode: authCodeQ?.value || null
+      }
+    }
+  }
+
   async createCustomer({customerId, customerName, trackingToken, clientId}) {
-    const apiKey = process.env[`TRACKIER_API_KEY_${clientId}`];
-    const authCode = process.env[`TRACKIER_AUTH_CODE_${clientId}`]
+    const keys = await this.getKeys(clientId);
 
-    const authres: any = await this.getAccessToken(authCode);
+    if (keys.success) {
+      const authres: any = await this.getAccessToken(keys.data.AuthCode);
 
-    if (!authres.success) return handleError(authres.error.message, null);
+      if (!authres.success) return handleError(authres.error.message, null);
 
-    return await axios.post(
-      `${this.baseUrl}/customer`,
-      {
-        customerId,
-        customerName,
-        date: dayjs().format('YYYY-MM-DD'),
-        timestamp: dayjs().unix(),
-        country: 'NG',
-        currency: 'ngn',
-        trackingToken,
-        productId: '1',
-      },
-      {
-        headers: {
-          'x-api-key': apiKey,
-          authorization: `BEARER ${authres.data.accessToken}`,
+      return await axios.post(
+        `${this.baseUrl}/customer`,
+        {
+          customerId,
+          customerName,
+          date: dayjs().format('YYYY-MM-DD'),
+          timestamp: dayjs().unix(),
+          country: 'NG',
+          currency: 'ngn',
+          trackingToken,
+          productId: '1',
         },
-      },
-    );
+        {
+          headers: {
+            'x-api-key': keys.data.ApiKey,
+            authorization: `BEARER ${authres.data.accessToken}`,
+          },
+        },
+      );
+    }
   }
 
   async registerAffiliate(user_details, user, hashedPassword, clientId) {
-    const apiKey = process.env[`TRACKIER_API_KEY_${clientId}`];
-    const authCode = process.env[`TRACKIER_AUTH_CODE_${clientId}`]
+    const keys = await this.getKeys(clientId);
 
-    const authres: any = await this.getAccessToken(authCode);
+    if (keys.success) {
+      const authres: any = await this.getAccessToken(keys.data.AuthCode);
 
-    if (!authres.success) return handleError(authres.error.message, null);
+      if (!authres.success) return handleError(authres.error.message, null);
 
-    await axios.post(
-            `${this.baseUrl}/affiliate/register`,
-      {
-        affiliate: {
-          name: `${user_details.firstName} ${user_details.lastName}`,
-          email: user_details.email,
-          password: hashedPassword,
-          company: user.username,
-          phone: user_details.phone,
+      await axios.post(
+              `${this.baseUrl}/affiliate/register`,
+        {
+          affiliate: {
+            name: `${user_details.firstName} ${user_details.lastName}`,
+            email: user_details.email,
+            password: hashedPassword,
+            company: user.username,
+            phone: user_details.phone,
+          },
         },
-      },
-      {
-        headers: {
-          'x-api-key': apiKey,
-          authorization: `BEARER ${authres.data.accessToken}`,
+        {
+          headers: {
+            'x-api-key': keys.data.ApiKey,
+            authorization: `BEARER ${authres.data.accessToken}`,
+          },
         },
-      },
-    );
+      );
+    }
   }
 
-  async getAccessToken(auth_code) {
+  async getAccessToken(auth_code: string) {
     const resp = await axios.post(
       `${this.baseUrl}/oauth/access-refresh-token`,
       {
