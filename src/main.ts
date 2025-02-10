9df33168738d416b4d73f9ab9b2056c7c1aca87d
Franklyn Nussie <godfather.franklyn@gmail.com>
2025-02-10 13:52:21 +0100

    modified registration report

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+/* eslint-disable prettier/prettier */
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {Transport} from "@nestjs/microservices";
 import {join} from "path";
 import { protobufPackage } from './proto/identity.pb';
+
 
 async function bootstrap() {
 
@@ -17,6 +19,11 @@ async function bootstrap() {
     },
   });  
 
+  console.log(`Identity service on ${process.env.GRPC_PORT}:${process.env.GRPC_HOST}`)
+
   await app.listen();
+
+  console.log("App running successfully");
 }
+
 bootstrap();
