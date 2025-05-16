d32f53291482a49e208d4bd00dfa23fa14ea1050
Franklyn Nussie <godfather.franklyn@gmail.com>
2025-05-16 11:10:41 +0100

    fix

--- src/common/cloudinaryService.ts
+++ src/common/cloudinaryService.ts
@@ -19,7 +19,7 @@ export class CloudinaryService {
     cloudinary.v2.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      api_secret: process.env.CLOUDINARY_API_SECRET.toString(),
     });
 
     this.logger = new Logger(CloudinaryService.name);
